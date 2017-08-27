0765b911b6100a92a7fb4ac7afeefb0d59106af5
Hugh Johnson <hugh@boilermaker.net>
2017-08-27 18:53:27 -0400

    modified:   client.js 	modified:   server.js

--- client.js
+++ client.js
@@ -1,124 +1,196 @@
-var ping = require("ping");
-var sqlite3 = require('sqlite3').verbose();
-var macaddress = require('node-macaddress');
+var _ = require('lodash');
+var SMB2 = require('smb2');
 
-// Get my network interface information
-function getMyInterface() {
-	return macaddress.all(function(err,mac){
-		if (err) throw err
-	//})['Local Area Connection'];
-	//})['Wi-Fi'];
-	});
-}
+var timers = {};
 
-console.log(getMyInterface())
+// initialize config client will receive from server
+var config = { 
+	"cspi"  : 2, // client-server poll interval in seconds
+	"smb" : [ ]
+};
+
+var postURL = "http://127.0.0.1:8008/plus";
 
 // Create database in memory and create tables if do not exist.
+var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database(':memory:');
-db.run("CREATE TABLE if not exists ping (date INTEGER, host TEXT, ipv4 TEXT, mac TEXT, ms INTEGER)");
-db.run("CREATE TABLE if not exists http (date INTEGER, url TEXT, ipv4 TEXT, mac TEXT)");
+db.run("CREATE TABLE if not exists smb (date INTEGER, share TEXT, domain TEXT, username TEXT, password TEXT)");
 
-// Poll server for config
-var config = { 
-	"cspi"  : 1, // client-server poll interval in seconds
-	"ping" : { "interval" : 5, "addresses": []},
-	"http" : { "interval" : 5, "addresses": []}
-};
-function getConfig() {
-	var request = require('request');
-	request('http://127.0.0.1:8008/config', function (error, response, body) {
-		config = JSON.parse(body);
-	});
-}
-
-// Ping hosts, record results to local DB
-function pingHosts() {
-	pingTimeout = setTimeout(pingHosts, config.ping.interval * 1000);
-	var myInterface = getMyInterface();
-	config.ping.addresses.forEach(function (host) {
-	    ping.promise.probe(host)
-	        .then(function (res) {
-	            //console.log([Date.now(),res.host,myInterface.ipv4,myInterface.mac,res.time]);
-	            db.run("insert into ping values (" + Date.now()  + ", '" +
-                                                     res.host + "', '" +
-                                                     myInterface.ipv4 + "', '" +
-                                                     myInterface.mac + "', " +
-                                                     ((res.time === 'unknown') ? "NULL" : res.time) + ")");
-	        })
-	        .done();
-	});
-}
-
-// Hits URL, record results to local DB
-function getURL() {
-	httpTimeout = setTimeout(getURL, config.http.interval * 1000);
-	var myInterface = getMyInterface();
-	config.http.addresses.forEach(function (url) {
-		var request = require('request');
-	    request.get({url:url}, function(e, r, user) {
-            db.run("insert into http values (" + Date.now()  + ", '" +
-                                                     url + "', '" +
-                                                     myInterface.ipv4 + "', '" +
-                                                     myInterface.mac + "')");
-            //console.log(r);
-	    })
-	});
-}
-
-//setInterval(pingHosts, 5000);
-//setInterval(getURL,    30000);
 
 var updateCspi = function(){
-	getPingsDB();
+	processDB();
     clearInterval(cspiInterval);
     cspiInterval = setInterval(updateCspi, config.cspi * 1000);
 }
 var cspiInterval = setInterval(updateCspi, config.cspi * 1000);
-var pingTimeout = setTimeout(pingHosts, config.ping.interval * 1000);
-var httpTimeout = setTimeout(getURL, config.http.interval * 1000);
 
-function getDbData(){
-	var data = {};
-	db.all("select * FROM ping", 
-        function (err, rows) {
-        	data.ping = rows;
-        	db.all("select * FROM http", 
-        		function (err, rows) {
-        			data.http = rows;     
-        			//console.log("DB: "+ JSON.stringify(data));   	
-        			console.log("DB: "+ data.ping.length + " " + data.http.length); 
-        		})
-        }
-    );
+// Posts the entire DB in JSON format.
+// Upon success, deletes posted rows and updates config
+function postDB(dbObj) {
+	var request = require('request');
+	request.post(	postURL,
+				  	{ json: dbObj },
+                  	function (error, response, body) {
+                  		console.log("POSTED BODY: " + JSON.stringify(dbObj))
+                  		//  if successful delete posted rows and update config
+    			  		if (!error && response.statusCode == 200) {
+        					console.log("RETURNED BODY: " + JSON.stringify(body));
+        					reConfig(response.body);
+        					delPosted(dbObj);
+    			    	}
+				  	}
+	);
 }
-setInterval(getDbData,5000);
 
-// Get ping records from local DB, post to server, delete from local DB
-function getPingsDB() {
-	db.all("select * FROM ping", 
-        function (err, rows) {
-        	//console.log(rows);
-        	var request = require('request');
-        	request.post(	'http://127.0.0.1:8008/ping',
-	    				  	{ json: rows },
-	                      	function (error, response, body) {
-	        			  		if (!error && response.statusCode == 200) {
-	            					//console.log("From http: " + JSON.stringify(body))
-	            					//console.log(rows.length);
-	            					//console.log(response.body);
-	            					config = response.body;
-	            					for (var i=0;i<rows.length;i++) {
-	            						//console.log(rows[i].ipv4);
-	            						//console.log(String(rows[i].ms));
-	            						db.run('delete from ping where date = '  + rows[i].date + ' AND ' +
-	            							                          'host = "' + rows[i].host + '" AND ' +
-	            							                          'ipv4 = "' + rows[i].ipv4 + '" AND ' + 
-	            							                          'mac  = "' + rows[i].mac  + '" AND ' +
-	            							                          'ms '       + (( rows[i].ms === null ) ? "is null" : " = " + rows[i].ms)
-	            						);
-	            					}
-	        			    	}
-	    				  	}
-			);
-    });
+// Process new config
+function reConfig(newConfig) {
+	// deep copy running config to comparison copy
+	var oldConfig = _.cloneDeep(config);
+
+					var addRows = _.difference(newConfig[ip_proto],oldConfig[ip_proto]);
+				console.log("addRows: " + addRows.length)
+
+	// did polling interval change?
+	// if not, do nothing
+	if ( oldConfig.cspi === newConfig.cspi ){
+		console.log( "SAME CSPI");
+	}
+	// if so, change polling interval on running config
+	// remove cspi from OLD and NEW config
+	else {
+		console.log( "DIFF CSPI");
+		config.cspi = newConfig.cspi;
+		delete newConfig.cspi;
+		delete oldConfig.cspi;
+	}
+	// did anything other than polling interval change?
+	// if not, do nothing
+	if ( _.isEqual(oldConfig, newConfig) ) {
+		console.log( "NO CHANGE");
+	}
+	else {
+		// if there was a change, check every protocol
+		for (var ip_proto in oldConfig) {
+			// are contents of protocol config equal?
+			// if equal, do nothing
+			if (_.isEqual(oldConfig[ip_proto],newConfig[ip_proto])) {
+				console.log("OBJ EQUAL")
+			}
+			else {
+				// In NEW config, but NOT in OLD.
+				var addRows = _.filter(newConfig[ip_proto], function(obj){ return !_.find(oldConfig[ip_proto], obj); });
+				console.log("addRows: " + addRows.length)
+
+				for(var i = 0; i < addRows.length; i++) {
+					var generate = require('nanoid/generate');
+					var id = generate('abcdefghijklmnopqrstuvwxyzABCDEJGHIJKLMNOPQRSTUVWXYZ', 16);
+					timers[id] = { ip_proto: ip_proto, param: addRows[i]};
+					//timers[id].timeoutID = setTimeout(procTimer.bind(null,id){}, 1000);
+				}
+
+				// in OLD config, but NOT in NEW
+				var delRows = _.filter(oldConfig[ip_proto], function(obj){ return !_.find(newConfig[ip_proto], obj); });
+				console.log("delRows: " + delRows.length);
+				for(var i = 0; i < delRows.length; i++) {
+					var delKey = _.findKey(timers, function(o) { return _.isEqual(o.param, delRows[i]); })
+					delete timers[delKey];
+					console.log("Deleted: " + delKey);
+				}
+
+				// in OLD config and NEW config   *****Dont have to do anything?*****
+				//var keepRows = _.filter(oldConfig[ip_proto], function(obj){ return _.find(newConfig[ip_proto], obj); });
+				//console.log("keepRows: " + keepRows.length)
+
+
+				//for (var i = 0; i < oldConfig[ip_proto].length; i++){
+					//console.log(oldConfig[ip_proto][i])
+				//}
+				console.log("OBJ NOT EQUAL")
+			}
+
+		}
+
+
+		console.log( "CHANGE");
+		config = newConfig;
+	}
+	//config = newConfig;
+	for (var id in timers) {
+		if ("timeoutID" in timers[id]) { console.log("Exists: " + id) }
+		else { 
+			console.log("Does not exist");
+			if (timers[id].ip_proto === "smb"){
+				procSMB(id);
+			}
+			else if (timers[id].ip_proto != "smb") {
+
+			}
+		 }
+	}
 }
+
+// Converts entire DB to JSON
+// Upon completion, calls postDB IOT post to server
+function processDB() {
+	const SqliteToJson = require('sqlite-to-json');
+	const exporter = new SqliteToJson({ client: db });
+	exporter.all(function (err, all) { 
+		//console.log(all)
+		postDB(all);
+	});
+}
+
+// Delete posted DB rows
+function delPosted(dbObj) {
+	// initialize an array of SQL commands
+	var sqlCMD = []
+	// foreach table in the database object
+	for (var table in dbObj) {
+		// foreach row in the table
+		for (var i = 0; i < dbObj[table].length; i++){
+			// initialize a SQL delete
+			sqlCMD[i] = 'delete from ' + table + ' where '
+			var c = 0;  // initialize counter
+			// foreach column in table			
+			for (var f in dbObj[table][i]) {
+				// prepend AND if not the first
+				if (c != 0){ sqlCMD[i] += ' AND '} 
+				// add 'column = value' to sql delete command 
+				sqlCMD[i] += f + ' = "' + dbObj[table][i][f] + '"'
+				c++;  // increment counter
+			}
+		}
+	}
+	// foreach SQL command
+	for (var i = 0; i < sqlCMD.length; i++){
+		console.log(sqlCMD[i])
+		// delete row
+		db.run(sqlCMD[i])
+	}
+}
+
+function procSMB(id) {
+	if (id in timers) {
+		var smb2Client = new SMB2({	share:    timers[id].param.share,
+								    domain:   timers[id].param.domain, 
+								    username: timers[id].param.username,
+								    password: timers[id].param.password} );
+		var filename = "file" + Date.now() + '.txt';
+		smb2Client.writeFile(filename, String(Date.now()), function (err) {
+    		if (err) throw err;
+    		smb2Client.readFile(filename, function(err, data){
+    			if(err) throw err;
+    			console.log(Date.now() + " SMB2 " + data);
+    		});
+		});
+		console.log("*******************************Timer: " + id)
+		timers[id].timeoutID = setTimeout(procSMB.bind(null,id), timers[id].param.interval * 1000)	
+	}
+}
+
+///////////////////////////////////////////
+
+setTimeout( function() {
+				db.run("insert into smb (date, share, domain, username, password) values (0, 'A', 'A', 'A', 'A')");
+				db.run("insert into smb (date, share, domain, username, password) values (1, 'B', 'B', 'B', 'B')");
+			}, 50 );
